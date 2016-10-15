f259e8b3475fbe406a45e388703df17e50cacc88
caasi Huang <caasi.igd@gmail.com>
2016-10-16 04:50:31 +0800

    WIP: Update the config

--- config.example.js
+++ config.example.js
@@ -1,5 +1,14 @@
-module.exports = {
+import path from 'path'
+
+export default {
+  GAPI: {
+    "secret_path": path.join(__dirname, "client_secret.json"),
+    "token_path": path.join(__dirname, ".gapi-token.json"),
+    "parent_dir": "",
+    "template": "",
+    "hub": ""
+  },
   TYPEFORM: {
     "hub": ""
   }
-};
+}

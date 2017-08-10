c896ee3a34129d0dc4ee97d0e264b748c7989b78
caasi Huang <caasi.igd@gmail.com>
2017-08-10 22:42:10 +0800

    Upgrade `webpack` and other packages

---
+++ webpack/client.config.js
@@ -0,0 +1,62 @@
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
+var sourcePath = path.join(__dirname, '../src');
+var targetPath = path.join(__dirname, '../');
+
+module.exports = {
+  entry: {
+    client: ['babel-polyfill', path.join(sourcePath, 'client/index.jsx')]
+  },
+  output: {
+    path: targetPath,
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: { context: sourcePath }
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    alias: { '~': sourcePath }
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            importLoaders: 1
+          }
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            parser: 'postcss-scss',
+            plugins: function () { return [precss, autoprefixer] }
+          }
+        }
+      ],
+      include: sourcePath
+    }]
+  }
+};
--- webpack/server.config.js
+++ webpack/server.config.js
@@ -2,8 +2,8 @@ var path = require('path');
 var webpack = require('webpack');
 var nodeExternals = require('webpack-node-externals');
 
-var sourcePath = path.join(__dirname, 'src');
-var targetPath = path.join(__dirname);
+var sourcePath = path.join(__dirname, '../src');
+var targetPath = path.join(__dirname, '../');
 
 module.exports = {
   entry: {
@@ -25,20 +25,16 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
       }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin()
+    })
   ],
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['.js']
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
-      loader: 'babel',
+      loader: 'babel-loader',
       exclude: /node_modules/
-    }, {
-      test: /\.json$/,
-      loaders: ['json']
     }]
   }
 };

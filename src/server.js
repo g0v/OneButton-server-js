27660980f18951d29dffcc80cf14c96b3cb780b0
caasi Huang <caasi.igd@gmail.com>
2017-08-11 21:59:44 +0800

    Update API URLs

--- src/server.js
+++ src/server.js
@@ -21,16 +21,16 @@ if (!module.parent) {
       app
         .use(bodyParser())
         .use(json())
-        .use(route.put('/typeform', typeform.put))
-        .use(route.get('/typeform', typeform.get))
-        .use(route.get('/typeform/:uid', typeform.getForm))
-        .use(route.get('/typeform/:uid/result', typeform.getFormResultList))
-        .use(route.get('/typeform/:uid/result/:token', typeform.getFormResult))
-        .use(route.post('/typeform', typeform.post))
-        .use(route.get('/oauth', function *() {
+        .use(route.put('/api/typeform', typeform.put))
+        .use(route.get('/api/typeform', typeform.get))
+        .use(route.get('/api/typeform/:uid', typeform.getForm))
+        .use(route.get('/api/typeform/:uid/result', typeform.getFormResultList))
+        .use(route.get('/api/typeform/:uid/result/:token', typeform.getFormResult))
+        .use(route.post('/api/typeform', typeform.post))
+        .use(route.get('/api/oauth', function *() {
           this.body = this.query;
         }))
-        .use(route.get('/slide', function *() {
+        .use(route.get('/api/slide', function *() {
           return send(this, this.path, { root: path.join(__dirname, '../slide') })
         }))
         .use(spa(path.join(__dirname, '../'), {
